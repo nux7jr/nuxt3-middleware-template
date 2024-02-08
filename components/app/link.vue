1f5dfaf7ffa8589f11603f759ec272a3e67c8bcc
nux7jr <nux7jr@gmail.com>
2024-02-08 09:23:35 +0700

    hotfix

--- components/app/link.vue
+++ components/app/link.vue
@@ -1,5 +1,5 @@
 <template>
-    <nuxt-link :to="'/' + cityCode + partnersIndex + props.to">
+    <nuxt-link :to="'/' + cityCode + partnersName + props.to">
         <slot></slot>
     </nuxt-link>
 </template>
@@ -10,7 +10,8 @@ const props = defineProps({
         default: '/'
     }
 })
+const appConfig = useAppConfig();
 const router = useRoute();
-const cityCode = router.params.city ? router.params.city : 'krasnoyarks'
-const partnersIndex = router.params.partner ? '/' + router.params.partner : ''
+const cityCode = router.params.city ? '/' + router.params.city : '/' + appConfig.url.baseCity
+const partnersName = router.params.partner ? '/' + router.params.partner : ''
 </script>
